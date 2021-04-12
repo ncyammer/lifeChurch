0600194f273ae47c49fb1eb1f2b1fdb791a8adf5
ncyammer <72411549+ncyammer@users.noreply.github.com>
2021-04-12 13:58:15 -0700

    Update2
    Adds JOYLand and SOF Page

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 80) {
+//           $(".customNav").css("background-color" , "#16181b !important");
+//         }
+//         else{
+//             $(".customNav").css("background-color" , "transparent !important");  	
+//         }
+//     })
+//   })
